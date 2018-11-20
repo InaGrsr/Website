502f5780a5d2cb6e7fc5f570b15686a22c9359da
InaGrsr <45176239+InaGrsr@users.noreply.github.com>
2018-11-20 18:14:08 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form action="index.php" method="get">
+    Name: <input type="text" name="name"> <br>
+    
+    Begrüßung: <select name="Begrüßung" size="1">
+    <option>Guten Morgen</option>
+    <option>Mahlzeit</option>
+    <option>Guten Abend</option>
+    <option>Gute Nacht</option>
+    </select>
+    
+<h1>I say YES! to...</h1>
+    
+    <select name="Unfug0" size="1">
+    <option>Unfug 0</option>
+    </select>
+    <select name="Unfug1" size="1">
+    <option>Unfug 1</option>
+    </select>
+    <select name="Unfug2" size="1">
+    <option>Unfug 2</option>   
+    </select>
+    <br>
+    
+    <input type="submit" value="Los geht's!">
+    
+    </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head> </head>
+<body> 
+    <?php echo $_GET['Begrüßung']; ?>
+    <?php echo $_GET['name']; ?>
+    
+    </body>
+</html>
